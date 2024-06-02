2697e5826c5751fd304e0745d10e1e538657a0d0
elias <g120000@yeah.net>
2024-06-02 18:56:46 +0800

    项目完成（部分功能可能由于api接口问题无法使用）

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBar />
+  <router-view :key="$router.fullpath"/>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped></style>
